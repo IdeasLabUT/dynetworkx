df66cfe4056674e0d660bed7406447e1afb3a46d
linkzero177 <hung.do@rockets.utoledo.edu>
2021-01-26 12:12:43 -0500

    update snapshotdigraph to be compatible with snapshot graph

--- dynetworkx/classes/snapshotdigraph.py
+++ dynetworkx/classes/snapshotdigraph.py
@@ -1,13 +1,11 @@
 from networkx.classes.digraph import DiGraph
 from networkx.classes.multidigraph import MultiDiGraph
 from dynetworkx.classes.snapshotgraph import SnapshotGraph
-import numpy as np
-from networkx import adjacency_matrix, from_numpy_array
 
 
 class SnapshotDiGraph(SnapshotGraph):
 
-    def add_snapshot(self, ebunch=None, graph=None, num_in_seq=None, multi=False):
+    def add_snapshot(self, ebunch=None, graph=None, start=None, end=None, time=None, multi=False):
         """Add a snapshot with a bunch of edge values.
 
         Parameters
@@ -17,8 +15,9 @@ class SnapshotDiGraph(SnapshotGraph):
             Each edge in the ebunch list will be included to all added graphs.
         graph : networkx graph object, optional (default= None)
             networkx graph to be inserted into snapshot graph.
-        num_in_seq : integer, optional (default= None)
-            Time slot to begin insertion at.
+        start: start timestamp, inclusive
+        end: end timestamp, exclusive
+        time: timestamp for impulses, cannot be used together with (start, end)
         multi : boolean, optional (default= False)
             Determines if type of graphs in snapshot are DiGraphs for MultiDiGraphs
 
@@ -40,10 +39,11 @@ class SnapshotDiGraph(SnapshotGraph):
         else:
             g = graph
 
-        if not num_in_seq:
-            num_in_seq = len(self.snapshots)
-
-        if num_in_seq > len(self.snapshots):
-            self.insert(g, snap_len=num_in_seq-len(self.snapshots)+1, num_in_seq=num_in_seq)
+        if time is not None and (start or end):
+            raise ValueError('Time and (start or end) cannot both be specified.')
+        elif time is not None:
+            self.insert(g, time=time)
+        elif (start is None and end is not None) or (start is not None and end is None):
+            raise ValueError('Start and end must both be specified for intervals.')
         else:
-            self.insert(g, snap_len=1, num_in_seq=num_in_seq)
+            self.insert(g, start=start, end=end)
